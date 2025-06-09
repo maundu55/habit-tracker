9f24641d8f5da406a8db34ae71377908a4aa6c13
maundu55 <marymaundu.mm@gmail.com>
2025-06-09 11:31:24 +0300

    adding icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,9 +1,11 @@
+import FontAwesome5 from '@expo/vector-icons/FontAwesome5';
 import { Tabs } from 'expo-router';
 
 export default function TabsLayout(){
   return (
-    <Tabs>
-      <Tabs.Screen name='index' options={{ title: 'Home' }} />
+    <Tabs screenOptions={{tabBarActiveTintColor:'coral'}}>
+      
+      <Tabs.Screen name='index' options={{ title: 'Home', tabBarIcon: ({color})=>(<FontAwesome5 name='home' size={20} color={color} />) }} />
       <Tabs.Screen name='explore' options={{ title: 'Explore' }} />
       <Tabs.Screen name='login' options={{ title: 'Login' }} />
     </Tabs>
